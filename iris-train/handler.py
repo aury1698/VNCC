068cac89d599be22181cec414f68d39a668e0526
nicolacucina <nicola.cucina@studenti.unipg.it>
2024-09-04 22:03:09 +0200

    aggiornamento per far vedere server deployato su kubernetes alla funzione openfaas

--- iris-train/handler.py
+++ iris-train/handler.py
@@ -4,6 +4,16 @@ import requests
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from kubernetes import client, config
+
+# Da usare dentro a kubernetes
+# config.load_kube_config()
+# Da usare con openfaas
+config.load_incluster_config()
+api = client.CoreV1Api()
+service = api.read_namespaced_service(name="iris-server-service", namespace="default")
+print('Service IP:', service.spec.cluster_ip)
+cluster_ip = service.spec.cluster_ip
 
 def handle(req):
     X, y = load_iris(return_X_y=True)
@@ -12,7 +22,8 @@ def handle(req):
     model.fit(X_train, y_train)
     model_bytes = pickle.dumps(model)
     # cambiare localhost con pinata
-    requests.post('http://localhost:6001/model', data=model_bytes)
+    # requests.post('http://localhost:6001/model', data=model_bytes)
+    result = requests.post('http://'+cluster_ip+':6002/model', data=model_bytes)
     if result.status_code >= 200 and result.status_code < 300:
         # print("Modello salvato.")
         return "Modello salvato."
