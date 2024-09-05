af4d2928e97091bd165209ff6a681a2d1b241677
nicolacucina <nicola.cucina@studenti.unipg.it>
2024-09-05 14:20:34 +0200

    aggiornato iris-predict per farlo funzionare con open-faas

--- iris-predict/handler.py
+++ iris-predict/handler.py
@@ -1,6 +1,13 @@
 import pickle
 import numpy as np
 import requests
+from kubernetes import client, config
+
+config.load_incluster_config()
+api = client.CoreV1Api()
+service = api.read_namespaced_service(name="iris-server-service", namespace="default")
+print('Service IP:', service.spec.cluster_ip)
+cluster_ip = service.spec.cluster_ip
 
 with open('model.pkl', 'rb') as f:
     default = pickle.load(f)
@@ -10,25 +17,29 @@ def handle(req):
         foo = req.split(';')
         type = foo[0]
         data = foo[1]
-        if type == 'default': 
+        if type == 'default':
+            print('default model') 
             # Convertiamo l'input in un array numpy (modifica questo in base al tuo modello)
-            input_data = np.array([float(i) for i in req.split(',')]).reshape(1, -1)
+            input_data = np.array([float(i) for i in data.split(',')]).reshape(1, -1)
             # Facciamo la predizione
             prediction = default.predict(input_data)
             # Restituiamo il risultato come stringa
             result = "Predizione:" + str(prediction[0])
             return result
         elif type == 'custom':
+            print('custom model')
             # cambiare localhost con pinata
-            model_bytes = requests.get('http://localhost:6001/model')
+            # model_bytes = requests.get('http://localhost:6001/model')
+            request = requests.get('http://'+cluster_ip+':6002/model')
+            model_bytes = request.content
             custom = pickle.loads(model_bytes)
             # Convertiamo l'input in un array numpy (modifica questo in base al tuo modello)
-            input_data = np.array([float(i) for i in req.split(',')]).reshape(1, -1)
+            input_data = np.array([float(i) for i in data.split(',')]).reshape(1, -1)
             # Facciamo la predizione
             prediction = custom.predict(input_data)
             # Restituiamo il risultato come stringa
             result = "Predizione:" + str(prediction[0])
-            return
+            return result
         else:
             return "Invalid type"
     except Exception as e:
